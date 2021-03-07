fe33f6008f6c542bd8e4f7ba2319a8cd4c08fb7e
cx4 <41991295+cx4@users.noreply.github.com>
2021-03-07 17:15:30 +0800

    Add files via upload

---
+++ api/iqiyi.py
@@ -0,0 +1,38 @@
+
+#coding:utf-8
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+import re
+url = "https://so.iqiyi.com/so/q_%E8%B5%98%E5%A9%BF"
+
+payload={}
+headers = {
+  'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+  'sec-ch-ua-mobile': '?0',
+  'Upgrade-Insecure-Requests': '1',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Sec-Fetch-Site': 'same-site',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-User': '?1',
+  'Sec-Fetch-Dest': 'document'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+content = response.content
+
+# soup = BeautifulSoup(content,'lxml')
+# album_list = soup.find_all('li',_class='album-list')
+# print(album_list) 
+html = etree.HTML(content)
+data = html.xpath('//*[@id="__layout"]/div/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/ul[2]/li/a')
+
+for i in data:
+  if 'iqiyi' in i.attrib.get('href'):
+    #print(i.attrib.get('href'))
+    order = i.attrib.get('title')
+    o = re.findall(r"\d+", order)
+    print(o)
+    print(i.attrib.get('href'))
